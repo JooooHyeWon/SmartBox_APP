de3881caa90d2101bb935a0c191bf3b2814be781
Hyewon <haha0140@naver.com>
2019-05-13 15:21:19 +0900

    Initial commit

---
+++ app/src/main/java/com/example/userss/gach/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.userss.gach;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // 로그인 화면 전환
+        Button log = (Button)findViewById(R.id.buttonLogin);
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        MainActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+        // 회원가입 화면 전환
+        Button sign = (Button)findViewById(R.id.buttonSignin);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        SigninActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/userss/gach/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.userss.gach;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    //view Objects
+    private Button buttonScan;
+    private TextView textViewName, textViewResult;
+
+    //qr code scanner object
+    private IntentIntegrator qrScan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 데이터 원본 준비
+        String[] items = {"자켓", "티셔츠", "바지", "치마"};
+
+        //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
+        ArrayAdapter<String> adapt
+                = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                items);
+
+        //어댑터 연결
+        ListView list = (ListView) findViewById(R.id.listView);
+        list.setAdapter(adapt);
+
+        // 테스트용 qr화면 이동
+        Button my = (Button)findViewById(R.id.mytest);
+        my.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        QRActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+        // 장바구니 화면 전환
+       Button shopping = (Button)findViewById(R.id.shoppingButton);
+        shopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        ShoppingActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+
+        // QR 인식
+        //View Objects
+        buttonScan = (Button) findViewById(R.id.qrButton);
+
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
+        //button onClick
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //scan option
+                qrScan.setPrompt("Scanning...");
+                //qrScan.setOrientationLocked(false);
+                qrScan.initiateScan();
+            }
+        });
+    }
+
+
+    // QR코드
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //qrcode 가 없으면
+            if (result.getContents() == null) {
+                Toast.makeText(MainActivity.this, "취소!", Toast.LENGTH_SHORT).show();
+            } else {
+                //qrcode 결과가 있으면
+                Toast.makeText(MainActivity.this, "스캔완료!", Toast.LENGTH_SHORT).show();
+                try {
+                    //data를 json으로 변환
+                    JSONObject obj = new JSONObject(result.getContents());
+                    // 스캔한 상품 이름만 toast 출력
+                    //Toast.makeText(MainActivity.this, obj.getString("name"), Toast.LENGTH_LONG).show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle(obj.getString("name"));
+                    builder.setMessage("위 상품을 장바구니에 담으시겠습니까?");
+                    builder.setIcon(android.R.drawable.ic_dialog_alert);
+                    builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(MainActivity.this, "장바구니에 담겼습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(MainActivity.this, "취소되었습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    textViewResult.setText(result.getContents());
+                }
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+    }
